8d7eb6d47218027e5cc1415a853cb6980d8243b0
torczuk <torczuk@gmail.com>
2012-11-29 23:42:34 +0100

    14th problem

---
+++ src/test/scala/com/torczuk/nintynine/DuplicateListsSuite.scala
@@ -0,0 +1,12 @@
+package com.torczuk.nintynine
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arek
+ * Date: 29.11.12
+ * Time: 23:31
+ * To change this template use File | Settings | File Templates.
+ */
+class DuplicateListsSuite {
+
+}
