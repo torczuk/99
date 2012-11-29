ea979122baeb5d6b91fd5cf2eb77e15ea5cd927c
torczuk <torczuk@gmail.com>
2012-11-29 22:10:19 +0100

    12th problem

---
+++ src/test/scala/com/torczuk/nintynine/DecodeListsSuite.scala
@@ -0,0 +1,12 @@
+package com.torczuk.nintynine
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arek
+ * Date: 29.11.12
+ * Time: 21:36
+ * To change this template use File | Settings | File Templates.
+ */
+class DecodeListsSuite {
+
+}
